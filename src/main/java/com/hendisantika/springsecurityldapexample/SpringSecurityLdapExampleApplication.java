9c76cc84537f41bd022112a8af728e23ae1be4a6
hendisantika <hendisantika@yahoo.co.id>
2019-04-03 05:47:49 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springsecurityldapexample/SpringSecurityLdapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springsecurityldapexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLdapExampleApplication.class, args);
+    }
+
+}
